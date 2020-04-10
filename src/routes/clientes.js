b93f32b0d3c27d625e7b6642808652e7ac5affae
alejo1026 <dahdhenao@gmail.com>
2020-04-09 19:58:51 -0500

    se deja todo el funcionamiento de creditostermiando

---
+++ src/routes/clientes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const crypto = require('crypto-js')
+const cryp = require ('crypto')
+const jwt = require('jsonwebtoken')
+
+
+module.exports = router;
